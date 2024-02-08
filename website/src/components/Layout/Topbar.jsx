f0dabf2cf4581b5b3406eceb10f417baed68d1af
FaithDaka <faithbusigu8@gmail.com>
2024-02-08 13:56:22 +0300

    feat: topbar component

---
+++ website/src/components/Layout/Topbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import LogoIcon from "assets/img/logo.png";
+import { Link } from "react-router-dom";
+
+const TopBarComponent = () => {
+  return (
+    <div className="Topbar">
+      <div className="container">
+        <Link to="/">
+          <div className="logo-div">
+            <img
+              src={LogoIcon}
+              alt="B Design Hub Logo"
+              width={100}
+            />
+            <span className="title">
+              <div>DESIGN</div>
+              <div>HUB</div>
+            </span>
+          </div>
+        </Link>
+        <div className="nav-div">
+          <div className="placeholder"></div>
+        </div>
+        <div className=""></div>
+      </div>
+    </div>
+  );
+};
+
+export default TopBarComponent;
