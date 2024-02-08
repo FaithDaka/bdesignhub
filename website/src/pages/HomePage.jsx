2777da5a5a849cbb91f5bce6c1e00af9fb7fdb95
FaithDaka <faithbusigu8@gmail.com>
2024-02-08 13:57:06 +0300

    feat: home page

--- website/src/pages/HomePage.jsx
+++ website/src/pages/HomePage.jsx
@@ -1,42 +1,19 @@
 import React from "react";
-import IconLogo from "assets/img/icon-logo.png";
+import PageLayoutComponent from "components/Layout/PageLayout";
+import HeroSectionComponent from "components/Homepage/HeroSection";
+import AboutSectionComponent from "components/Homepage/AboutSection";
+import ConceptsComponent from "components/Homepage/Concepts";
 
 const HomePage = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "600px",
-        textAlign: "center",
-      }}
-    >
-      <div>
-        <div>
-          <img src={IconLogo} alt="" />
-        </div>
-        <div>
-          <h1 style={{ color: "#4E1757" }}>
-            This site is currently undergoing an overhaul...
-          </h1>
-        </div>
-        <div>
-          <h3 style={{ color: "#2d2d2d" }}>Exciting Looks coming up!</h3>
-        </div>
-        <div>
-          <h5>
-            For more info, send an email to:{" "}
-            <a
-              style={{ fontSize: "16px", color: "#8D0EA2" }}
-              href="mailto:info@bdesignhub.com"
-            >
-              info@bdesignhub.com
-            </a>
-          </h5>
-        </div>
+    <>
+      <PageLayoutComponent></PageLayoutComponent>
+      <div className="Homepage">
+        <HeroSectionComponent/>
+        <AboutSectionComponent/>
+        <ConceptsComponent/>
       </div>
-    </div>
+    </>
   );
 };
 
