3364984150c535d2d2b42a6e7767f75f59dcd7f2
FaithDaka <faithbusigu8@gmail.com>
2024-02-09 16:40:59 +0300

    feat: freestyle section

---
+++ website/src/components/Homepage/FreestyleSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Illustration from 'assets/svg/free-style.svg'
+
+const FreestyleSectionComponent = () => {
+  return (
+    <div className='free-section'>
+        <div className='container'>
+            <img src={Illustration} alt='' width={1000}/>
+        </div>
+    </div>
+  )
+}
+
+export default FreestyleSectionComponent
