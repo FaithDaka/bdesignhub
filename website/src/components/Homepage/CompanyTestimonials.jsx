4bd3043b57effdff27eff1c7e75cde861da53b00
FaithDaka <faithbusigu8@gmail.com>
2024-05-20 14:20:32 +0300

    feat: company logos section

---
+++ website/src/components/Homepage/CompanyTestimonials.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import FirstLogo from "assets/svg/Company Logos/firstlogo.svg"
+import SecondLogo from "assets/svg/Company Logos/secondlogo.svg";
+import ThirdLogo from "assets/svg/Company Logos/thirdlogo.svg";
+import FourthLogo from "assets/svg/Company Logos/fourthlogo.svg";
+import FifthLogo from "assets/svg/Company Logos/fifthlogo.svg";
+
+const CompanyTestimonials = () => {
+  return (
+    <div className="company-logos">
+      <div className="title">
+        <h1>TRUSTED BY HOMEGROWN COMPANIES</h1>
+      </div>
+      <div className="logos">
+        <div className="logo">
+          <img src={FirstLogo} alt="" />
+        </div>
+        <div className="logo">
+          <img src={SecondLogo} alt="" />
+        </div>
+        <div className="logo">
+          <img src={ThirdLogo} alt="" />
+        </div>
+        <div className="logo">
+          <img src={FourthLogo} alt="" />
+        </div>
+        <div className="logo">
+          <img src={FifthLogo} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CompanyTestimonials;
