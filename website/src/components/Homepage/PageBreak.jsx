610eb49cde1580a3b62b9ca9e5fa95470e95adf4
FaithDaka <faithbusigu8@gmail.com>
2024-05-21 10:09:57 +0300

    feat: page break component

---
+++ website/src/components/Homepage/PageBreak.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PageBreak from "assets/svg/PageBreak/Page Break.svg";
+
+const PageBreakComponent = () => {
+  return (
+    <div className="page-break-section">
+      <div className="container">
+        <img src={PageBreak} alt=""/>
+      </div>
+    </div>
+  );
+};
+
+export default PageBreakComponent;
