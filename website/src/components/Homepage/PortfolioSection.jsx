18cdd3954be2df9b19ffb0372d8355e0723e1426
FaithDaka <faithbusigu8@gmail.com>
2024-05-21 12:04:18 +0300

    feat: Portfolio Section

---
+++ website/src/components/Homepage/PortfolioSection.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import TopImage from "assets/svg/Portfolio Section/Accessibility.svg"
+import LeftImage from "assets/svg/Portfolio Section/Aesthetics.svg";
+import RightImage from "assets/svg/Portfolio Section/Responsive.svg";
+import ListImage from "assets/svg/Portfolio Section/Rightllustration.svg";
+import { Link } from 'react-router-dom';
+
+const PortfolioSectionComponent = () => {
+  return (
+    <div className="portfolio-section">
+      <div className="container">
+        <div className="section-label">/ PORTFOLIO</div>
+        <div className="content">
+          <div className="illustrations">
+            <div className='top-image'><img src={TopImage} alt="" /></div>
+            <div className='bottom-images'>
+              <img src={LeftImage} alt="" />
+              <img src={RightImage} alt=""/>
+            </div>
+          </div>
+          <div className='portfolio-list'>
+            <Link to="#">
+            <img src={ListImage} alt=''/>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PortfolioSectionComponent
