c7aba17a353a3831a72ca40a0b7bbe78a78c10dd
FaithDaka <faithbusigu8@gmail.com>
2024-05-23 09:24:20 +0300

    feat: about page header

---
+++ website/src/components/Aboutpage/HeaderComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div className='about-header'>
+        <div className="container">
+            <h2>
+                <span className="purple-text">About </span>
+                <span className="yellow-text">B Design Hub</span>
+            </h2>
+        </div>
+    </div>
+  )
+}
+
+export default HeaderComponent
