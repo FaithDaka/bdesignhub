b0a40771c5b81831f8dce7bcdb42b9e08325f3e5
FaithDaka <faithbusigu8@gmail.com>
2026-08-11 18:43:11 +0300

    feat: topbar timezone and email

--- website/src/components/Layout/Topbar.jsx
+++ website/src/components/Layout/Topbar.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const TopBarComponent = () => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => setDate(new Date()), 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const UG = "Africa/Nairobi";
+  const hour = parseInt(
+    date.toLocaleString("en-US", {
+      timeZone: UG,
+      hour: "numeric",
+      hour12: false,
+    }),
+    10,
+  );
+  const formattedTime = date.toLocaleTimeString("en-US", {
+    timeZone: UG,
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  });
+
+  const AVAILABLE_FROM = 8; // 8 AM
+  const AVAILABLE_TO = 17; // UPTO 6PM
+
+  const isAvailable = hour >= AVAILABLE_FROM && hour <= AVAILABLE_TO;
+
   return (
     <div className="Topbar">
       <div className="container">
@@ -24,6 +52,21 @@ const TopBarComponent = () => {
           {/* <div className="navlink">
             <Link to="/contact"> Consultation</Link>
           </div> */}
+        </div>
+        <div className="avail">
+          <div className="label">
+            {isAvailable ? (
+              <span className="time online">
+                AVAIL <span className="dot"></span> <span>{formattedTime}</span>
+              </span>
+            ) : (
+              <span className="time offline">
+                UNAVAIL <span className="dot"></span> <span>{formattedTime}</span>
+              </span>
+            )}
+            <span></span>
+          </div>
+          <div className="email">faithux.co@gmail.com</div>
         </div>
       </div>
     </div>
