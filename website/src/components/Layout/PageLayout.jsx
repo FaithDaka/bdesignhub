07487a64ad502d12e44685d63d1bb38d70eedc37
FaithDaka <faithbusigu8@gmail.com>
2026-08-14 10:22:15 +0300

    refactor: add email dialog box

--- website/src/components/Layout/PageLayout.jsx
+++ website/src/components/Layout/PageLayout.jsx
@@ -1,15 +1,21 @@
 import TopBarComponent from "components/Layout/Topbar";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import FooterComponent from "./Footer";
 import ContactDetailsComponent from "./ContactDetails";
+import { EmailDialog } from "./EmailDialog";
 
 const PageLayoutComponent = ({ children, contactBackground }) => {
+  const [emailOpen, setEmailOpen] = useState(false);
+
+  const openEmail = useCallback(() => setEmailOpen(true), []);
+  const closeEmail = useCallback(() => setEmailOpen(false), []);
   return (
     <div className="Page">
-      <TopBarComponent />
+      <TopBarComponent onEmailClick={openEmail} />
       {children}
-      <ContactDetailsComponent backgroundColor={contactBackground}/>
+      <ContactDetailsComponent backgroundColor={contactBackground} />
       <FooterComponent />
+      <EmailDialog open={emailOpen} onClose={closeEmail} />
     </div>
   );
 };
