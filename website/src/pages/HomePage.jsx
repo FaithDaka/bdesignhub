2d932d008998aece3ad411b16aa79b13e4b23e69
FaithDaka <faithbusigu8@gmail.com>
2026-08-25 14:00:10 +0300

    feat: homepage v3

--- website/src/pages/HomePage.jsx
+++ website/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import PageLayoutComponent from "components/Layout/PageLayout";
 import HeroSectionComponent from "components/Homepage/HeroSection";
 import AboutSectionComponent from "components/Homepage/AboutSection";
@@ -8,20 +8,26 @@ import PageBreakComponent from "components/Homepage/PageBreak";
 import PortfolioSectionComponent from "components/Homepage/PortfolioSection";
 import PriceModelsComponent from "components/Homepage/PriceModels";
 import { useInitScrollTop } from "util/customHooks";
+import { EmailDialog } from "components/Layout/EmailDialog";
 
 const HomePage = () => {
   useInitScrollTop();
+  const [emailOpen, setEmailOpen] = useState(false);
+
+  const openEmail = useCallback(() => setEmailOpen(true), []);
+  const closeEmail = useCallback(() => setEmailOpen(false), []);
   return (
     <>
       <PageLayoutComponent contactBackground={"#F1F1F1"}>
         <div className="Homepage">
           <HeroSectionComponent />
-          <PortfolioSectionComponent />
+          <AboutSectionComponent openLink={openEmail} />
           <CompanyTestimonials />
-          <AboutSectionComponent />
-          <ServicesComponent />
+          <ServicesComponent onCtaClick={openEmail} />
+          <PortfolioSectionComponent onContactClick={openEmail} />
           {/* <PageBreakComponent /> */}
           {/* <PriceModelsComponent /> */}
+          <EmailDialog open={emailOpen} onClose={closeEmail} />
         </div>
       </PageLayoutComponent>
     </>
